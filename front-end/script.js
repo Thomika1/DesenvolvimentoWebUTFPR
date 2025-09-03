89ee7d0192cf5a9ab4cb3d7bf219ecfc747d70db
Thomika1 <thomazmbonfim@gmail.com>
2025-09-03 20:47:06 -0300

    feat: added btn effects and initial logic for chat inputs

--- front-end/script.js
+++ front-end/script.js
@@ -1,7 +1,8 @@
 const loginBtn = document.getElementById('login-btn');
 const loginPage = document.getElementById('login-page');
 const chatPage = document.getElementById('chat-page');
-
+const enviarBtn = document.getElementById('enviar-btn');
+const inputChat = document.getElementById('input-chat');
 
 loginBtn.addEventListener('click', (event) => {
     // Evita que o formulário seja submetido e recarregue a página
@@ -11,3 +12,11 @@ loginBtn.addEventListener('click', (event) => {
     loginPage.classList.add('d-none');
     chatPage.classList.remove('d-none');
 });
+
+enviarBtn.addEventListener('click', (event)=>{
+    event.preventDefault();
+
+    const text = inputChat.value;
+    inputChat.value = "";
+
+})  
